241995fae93277b0807305bb89b368b5f71d0432
GIND <guillermo6001@gmail.com>
2024-12-29 08:13:21 -0400

    se agregó CORS headers en la edge function

---
+++ supabase/functions/_shared/cors.ts
@@ -0,0 +1,5 @@
+export const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',  // Permite todas las fuentes
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  }
